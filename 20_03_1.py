29468525da6ea35c2f213d934397b3ef0ee70ef2
Lisaeki <m6830308@gmail.com>
2024-04-03 12:37:46 +0300

    Задание 6

--- 20_03_1.py
+++ 20_03_1.py
@@ -1,28 +1,28 @@
-#
-# def num1():
-#     s = int(input("Введите число: "))
-#     def n1(a):
-#         if a % 5 == 0:
-#             return "Число делится на 5!"
-#         else:
-#             return "Число не делится на 5."
-#     print(n1(s))
-# num1()
-#
-# def num2():
-#     k = input("Введите число: ")
-#     def n2(b):
-#         try:
-#             k == int(k)
-#             if int(k) == 0:
-#                 return "ZeroDivisionError"
-#             else:
-#                 b = int(k) / 300
-#                 return b
-#         except:
-#             return "ValueError"
-#     print(n2(k))
-# num2()
+
+def num1():
+    s = int(input("Введите число: "))
+    def n1(a):
+        if a % 5 == 0:
+            return "Число делится на 5!"
+        else:
+            return "Число не делится на 5."
+    print(n1(s))
+num1()
+
+def num2():
+    k = input("Введите число: ")
+    def n2(b):
+        try:
+            k == int(k)
+            if int(k) == 0:
+                return "ZeroDivisionError"
+            else:
+                b = int(k) / 300
+                return b
+        except:
+            return "ValueError"
+    print(n2(k))
+num2()
 
 def num3():
     import datetime as DT
