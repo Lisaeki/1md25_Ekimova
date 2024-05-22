97e187aeec23811a4166b1bc88cee88a09becacb
Lisaeki <m6830308@gmail.com>
2024-05-22 13:00:44 +0300

    Занятие 4

--- 20_03_1.py
+++ 20_03_1.py
@@ -2,10 +2,10 @@
 def num1():
     s = int(input("Введите число: "))
     def n1(a):
-        if a % 5 == 0:
-            return "Число делится на 5!"
+        if a % 3 == 0:
+            return "Число делится на 3!"
         else:
-            return "Число не делится на 5."
+            return "Число не делится на 3."
     print(n1(s))
 num1()
 
@@ -17,7 +17,7 @@ def num2():
             if int(k) == 0:
                 return "ZeroDivisionError"
             else:
-                b = int(k) / 300
+                b = 100 / int(k)
                 return b
         except:
             return "ValueError"
@@ -28,21 +28,37 @@ def num3():
     import datetime as DT
     l = input("Введите дату (dd/mm/yyyy): ")
     v = DT.datetime.strptime(l,'%d/%m/%Y').date()
-    print(v)
+    n = str(v)
+    day = n[-2:]
+    month = n[-5:-3]
+    year = n[2:4]
+    if int(day) * int(month) == int(year):
+        print("True")
+    else:
+        print("False")
 num3()
 
 def num4():
+    u = 0
+    n = 0
+    b = 0
     m = input("Введите число: ")
-    if len(m) % 2 != 0:
+    if int(len(m)) % 2 != 0:
         return False
-    def n4():
-        f = len(m) // 2
-        first = m[:f]
-        second = m[f:]
-        return sum(map())
-    if first == second:
-        print("Билет счастливый!")
     else:
-        print("Несчастливый билет(")
+        f = int(len(m)) // 2
+        k = m[:f]
+        s = m[f:]
+        while u < f:
+            n = n + int(m[u])
+            u = u + 1
+        i = f
+        while i < (2*f):
+            b = b + int(m[i])
+            i = i + 1
+    if n == b:
+        print("Счастливый билет")
+    else:
+        print("Несчастливый билет")
 num4()
 
